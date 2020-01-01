9ba8d5969bf8c94bb5edbbce3a7a15f4f1d19626
Fahdkamal101 <fahd.bsse3334@iiu.edu.pk>
2020-01-01 23:59:04 +0500

    updated

--- models/books.model.js
+++ models/books.model.js
@@ -3,26 +3,26 @@ const mongoosePaginate = require('mongoose-paginate');
 const Schema = mongoose.Schema;
 
 const Book = new Schema({
-     name: {
-        type: String
-    },
-     ibn: {
-        type: String,
-        unique: true,
-        sparse:true
-    },
+    name: {
+       type: String
+   },
+    ibn: {
+       type: String,
+       unique: true,
+       sparse:true
+   },
 
-     author: {
-        type: String
-    },
+    author: {
+       type: String
+   },
 
-    image_url: {
-        type: String
-    },
-    is_deleted: {
-        type: Boolean,
-        default: false
-    }
+   image_url: {
+       type: String
+   },
+   is_deleted: {
+       type: Boolean,
+       default: false
+   }
 });
 
 Book.plugin(mongoosePaginate);
