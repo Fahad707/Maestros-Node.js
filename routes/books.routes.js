5087d44257aaa714f19d23716af9c00e43ef46bd
Fahdkamal101 <fahd.bsse3334@iiu.edu.pk>
2020-01-02 00:06:44 +0500

    deletion route added

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -7,7 +7,7 @@ router.get("/",bookController.getAll);
 router.post("/add",bookController.addBook);
 // router.get("0/:_id",bookController.getSingleBook);
 router.put("/:_id", bookController.updateBook);
-//router.delete("/:_id", bookController.deleteBook);
+router.delete("/:_id", bookController.deleteBook);
 
 
 module.exports = router;
